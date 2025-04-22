b405b3b0a64fd45d2d1824dd386ce0c2f6643456
phamphanphuong <23210266@ms.uit.edu.vn>
2025-04-22 22:01:21 +0700

    'ok'

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+import Banner from './components/Banner'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <Header />
+      <Banner />
+     </div>
+    </>
+  )
+}
+
+export default App
